23d40cbbd5d9c767ae15a05a7553e835b4d96e39
thallis-andre <thallis-andre@hotmail.com>
2024-01-22 20:43:10 -0300

    fix: remove console.log

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,6 @@ import TimerComponent from '../src/components/Timer';
 import Tilt from 'react-parallax-tilt';
 
 const Home: NextPage = () => {
-  console.log('%cby BrenoHA', 'color: #4f4f; font-family:monospace; font-size: 22px');
 
   return (
     <>
