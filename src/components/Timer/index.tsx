593036b91ffd06007a093c9c4557ab46127188a0
thallis-andre <thallis-andre@hotmail.com>
2025-04-13 21:51:07 -0300

    .

--- src/components/Timer/index.tsx
+++ src/components/Timer/index.tsx
@@ -16,7 +16,7 @@ type TimerType = {
 
 const TimerComponent = () => {
   const calculateTimeLeft = () => {
-    const difference = +new Date(`2025-04-26T23:59:59`) - +new Date(); //YYYY-MM-DD
+    const difference = +new Date(`2025-04-12T23:59:59`) - +new Date(); //YYYY-MM-DD
     let timeLeft = {} as TimerType;
 
     if (difference > 0) {
@@ -65,9 +65,9 @@ const TimerComponent = () => {
     <div className={styles.container}>
       {timerEnded ? (
         <>
-          <Image src={finalImage} alt="Foto final" width={130} height={250} />
+          <Image src={finalImage} alt="Foto final" width={200} height={300} />
           <div className={styles.finalText}>
-            <span>Sou extremamente feliz por ter você como namorada</span>
+            <span>Sou extremamente feliz por ter você como namorada </span>
           </div>
         </>
       ) : (
